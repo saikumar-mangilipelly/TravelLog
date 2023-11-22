a3f437c124c4f04c16ad13a464b584df24a8da24
Saikumar Mangilipelly <saikumarmangilipelly3@gmail.com>
2023-11-22 14:21:30 +0530

    updated

--- server.js
+++ server.js
@@ -1,54 +1,54 @@
-const exp=require('express')
-const app=exp()
-const mongoclient=require("mongodb").MongoClient
+const exp = require('express')
+const app = exp()
+const mongoclient = require("mongodb").MongoClient
 require("dotenv").config()
 //importing path
-const path=require('path')
+const path = require('path')
 
 //connecting to react application
-app.use(exp.static(path.join(__dirname,'./build')))
+app.use(exp.static(path.join(__dirname, './build')))
 
 app.use(exp.json())
 
 //connecting database
 mongoclient.connect(process.env.DB_connection_url)
-.then((client)=>{
+    .then((client) => {
         // getting db 
-        let db_obj=client.db("travellog")
+        let db_obj = client.db("travellog")
         //creating collections
-        let usercollection=db_obj.collection("usercollection")
-        let mapcollection=db_obj.collection("mapcollection")
+        let usercollection = db_obj.collection("usercollection")
+        let mapcollection = db_obj.collection("mapcollection")
         //sharing collections to api's
-        app.set("usercollection",usercollection)
-        app.set("mapcollection",mapcollection)
+        app.set("usercollection", usercollection)
+        app.set("mapcollection", mapcollection)
         console.log("DB Connection successful !!..")
-})
-.catch(err=>console.log("err in database connection",err))
+    })
+    .catch(err => console.log("err in database connection", err))
 
 //importing userapi
-const userapi=require('../Travellog/APIS/Userapi')
-const mapapi=require('../Travellog/APIS/mapapi')
+const userapi = require('../TravelLog/APIS/Userapi')
+const mapapi = require('../TravelLog/APIS/mapapi')
 //executing middleware according to path
-app.use('/user',userapi)
-app.use('/mappin',mapapi)
+app.use('/user', userapi)
+app.use('/mappin', mapapi)
 //handling the page refresh
-app.use('*',(request,response)=>{
-    response.sendFile(path.join(__dirname,'./build/index.html'))
+app.use('*', (request, response) => {
+    response.sendFile(path.join(__dirname, './build/index.html'))
 })
 
 
 //middleware for handling invalid paths
-app.use((request,response)=>{
+app.use((request, response) => {
     console.log(error)
-    response.send({message:"error occured",reason:`Invalid url ${request.url}`})
+    response.send({ message: "error occured", reason: `Invalid url ${request.url}` })
 })
 
 
 //middleware for handling errors
-app.use((error,request,response,next)=>{
+app.use((error, request, response, next) => {
     console.log(error)
-    response.send({message:"Error occured",reason:`${error.message}`})
+    response.send({ message: "Error occured", reason: `${error.message}` })
 })
 
 //running server
-app.listen(process.env.Port,()=>console.log(`server is running on port no : ${process.env.Port}....`))
+app.listen(process.env.Port, () => console.log(`server is running on port no : ${process.env.Port}....`))
